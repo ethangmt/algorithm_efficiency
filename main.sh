921a79f679683bab34e3b1b963e931334c2e951d
ethangmt <ethang@live.com.mx>
2017-08-16 12:30:46 -0500

    main.sh, tester

--- main.sh
+++ main.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#Compile everything
-javac */*/*/*.java
+javac java_implementation/tools/*.java
+javac java_implementation/array/*/*.java
+javac java_implementation/linked_list/*/*.java
+javac java_implementation/data_structures/data_structures.java
+javac java_implementation/tester/tester.java
 
-#Run tests
+java java_implementation/data_structures/data_structures
 java java_implementation/tester/tester
-java java_implementation/data_structures
 
-#Create graphs
 python graphs.py
